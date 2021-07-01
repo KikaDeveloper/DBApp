d789c3108e76832a0f9729693cdab59b45b43791
kika <kikamozga18@ya.ru>
2021-07-01 16:42:30 +0300

    intermediate changes

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,22 +1,43 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+
+using System;
 
 namespace DBApp
 {
     public partial class MainWindow : Window
     {
+
+        TextBox Name {get; set;}
+        TextBox Url{get; set;}
+        TextBox Description {get; set;}
+
+
         public MainWindow()
         {
             InitializeComponent();
-#if DEBUG
+            #if DEBUG
             this.AttachDevTools();
-#endif
+            #endif
+
+            Name = this.Find<TextBox>("NameField");
+            Url = this.Find<TextBox>("UrlField");
+            Description = this.Find<TextBox>("DescriptionField");
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        public void OnButtonClick(object sender, RoutedEventArgs e){
+            Console.WriteLine("Click");
+            Console.WriteLine(Name.Text);
+            Console.WriteLine(Url.Text);
+            Console.WriteLine(Description.Text);
+        }
+
     }
 }
