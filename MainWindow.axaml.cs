9c4c44a668cf988903059ae528de6f16b0388fe8
kika <kikamozga18@ya.ru>
2021-07-02 09:55:42 +0300

    added eventHandler

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,18 +1,23 @@
 using Avalonia;
+using Avalonia.Input;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Interactivity;
 
 using System;
+using System.Threading.Tasks;
+
+using Helper;
 
 namespace DBApp
 {
     public partial class MainWindow : Window
     {
+        TextBox NameField {get; set;}
+        TextBox UrlField{get; set;}
+        TextBox DescriptionField {get; set;}
 
-        TextBox? NameField {get; set;}
-        TextBox? UrlField{get; set;}
-        TextBox? DescriptionField {get; set;}
+        Driver MongoDriver {get; set;} 
 
         public MainWindow()
         {
@@ -22,13 +27,13 @@ namespace DBApp
             this.AttachDevTools();
             #endif
 
-            InitializeControls();
-        }
-
-        private void InitializeControls(){
+            MongoDriver = new Driver("mongodb://localhost:27017", "data");
             NameField = this.Find<TextBox>("NameField");
             UrlField = this.Find<TextBox>("UrlField");
             DescriptionField = this.Find<TextBox>("DescriptionField");
+
+            // NameField.TextInput += GotFocusNameField;
+            // UrlField.TextInput += GotFocusUrlField;
         }
 
         private void InitializeComponent()
@@ -38,7 +43,31 @@ namespace DBApp
 
         public void OnButtonClick(object sender, RoutedEventArgs e){
             Console.WriteLine("Click");
-            Console.WriteLine($"{NameField?.Text} : {UrlField?.Text} : {DescriptionField?.Text}");
+            Console.WriteLine($"{NameField.Text} : {UrlField.Text} : {DescriptionField.Text}");
+
+            if(CheckFields()){
+                var item = new Item(NameField.Text, UrlField.Text, DescriptionField.Text);
+                MongoDriver?.AddDocument(item);
+            }            
+        }
+
+        public void GotFocusNameField(object sender, TextInputEventArgs e){
+            Console.WriteLine("Typing text");
+            if(NameField.Text != null || NameField.Text != ""){
+                this.Find<Label>("LabelName").Classes.Remove("normal");
+                this.Find<Label>("LabelName").Classes.Add("right");
+            } else {
+                this.Find<Label>("LabelName").Classes.Remove("right");
+                this.Find<Label>("LabelName").Classes.Add("wrong");
+            }
+        }
+
+        public void GotFocusUrlField(object sender, TextInputEventArgs e){
+
+        }
+
+        private bool CheckFields(){
+            return ((NameField.Text != "" || NameField.Text != "") && (UrlField.Text != " " || UrlField.Text != ""));
         }
 
     }
